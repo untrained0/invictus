ccb1316bfe84118e8961b3df8f05c44e8ccc85d7
Rishi Kokil <rishikokil@gmail.com>
2024-01-20 14:43:49 +0530

    Added navlinks

---
+++ frontend/src/Constants/Navlinks.js
@@ -0,0 +1,27 @@
+const navlinks = [
+    {
+        id: 1,
+        title: "Home",
+        url: "/",
+    },
+    {
+        id: 2,
+        title: "Community",
+        url: "/community",
+    }, 
+    {
+        id: 3,
+        title: "Resources",
+        url: "/resources",
+    }, 
+    {
+        id: 4,
+        title: "Financial Assistance",
+        url: "/financial-assistance",
+    }, 
+    {
+        id: 5,
+        title: "Contact Us",
+        url: "/contact-us",
+    },
+]
